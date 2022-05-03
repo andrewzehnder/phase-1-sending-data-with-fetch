dd1efca91fa2c07f140fd7a74e2239fda374aa1f
Andrew <azehnder2@gmail.com>
2022-05-03 14:41:27 -0700

    commit tree 23d45a9800ccd3407b90a8a8ce89e358b0dcf34a

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const formData = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+}
+
+
+const createPost = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        },
+    body: JSON.stringify(formData)  
+}
+
+function submitData() {
+    return fetch('http://localhost:3000/users', createPost)
+    .then(resp => resp.json())
+    .then(object => {
+        const newID = object.id;
+        const p = document.createElement('p');
+        p.textContent = newID;
+        const body = document.querySelector('body');
+        body.append(p);
+    })
+    .catch(error => {
+        const p = document.createElement('p');
+        p.textContent = error.message;
+        const body = document.querySelector('body');
+        body.append(p);
+    })
+}
